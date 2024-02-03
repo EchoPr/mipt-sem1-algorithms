18fce252567d2ebef9efa629677b51f6fb2742b4
EchoPr <arkasha.pronin.2005@gmail.com>
2024-02-03 19:16:40 +0300

    added hw1 matrix 4x4

---
+++ algo-s2-hw1/main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include "src/matrix.hpp"
+
+int main(){
+    Matrix<int> m1;
+    Matrix<int> m2(3);
+    Matrix<int> m3(1);
+    Matrix<float> m4(m2);
+
+    // m2(0, 0) = 4;
+    // m2(0, 3) = 4;
+    // std::cout << m2 << std::endl;
+    // std::cout << m2.transpose() << std::endl;
+
+    // m2.transpose();
+    // std::cout << m2.inverse() << std::endl;
+
+    for(int i = 0; i < ROWS; i++)
+        m2(i, i) = i + 5;
+    std::cout << m2.inverse() << std::endl;
+
+    for(int i = 0; i < ROWS; i++)
+        m1(i, i) = 2;
+    std::cout << m1.inverse() << std::endl;
+
+
+
+    
+}
